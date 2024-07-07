df5f858859253596682f2c5d20f20fff7682c2fe
yash11jain <yash.21bcon538@jecrcu.edu.in>
2024-07-07 22:39:28 +0530

    Add files via upload

---
+++ home3.py
@@ -0,0 +1,56 @@
+import os
+import streamlit as st
+from streamlit_navigation_bar import st_navbar
+import pages as pg
+
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+pages = ["About","User Guide"]
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(parent_dir, "cubes.svg")
+#urls = {"GitHub": "https://github.com/gabrieltempass/streamlit-navigation-bar"}
+styles = {
+    "nav": {
+        "background-color": "#0C3D5D",
+        "justify-content": "left",
+    },
+    "img": {
+        "padding-right": "14px",
+    },
+    "span": {
+        "color": "white",
+        "padding": "14px",
+    },
+    "active": {
+        "background-color": "white",
+        "color": "var(--text-color)",
+        "font-weight": "normal",
+        "padding": "14px",
+    }
+}
+options = {
+    "show_menu": False,
+    "show_sidebar": False,
+}
+
+page = st_navbar(
+    pages,
+    logo_path=logo_path,
+    #urls=urls,
+    styles=styles,
+    options=options,
+)
+
+functions = {
+    "Home": pg.show_home,
+    "Install": pg.show_install,
+    "User Guide": pg.show_user_guide,
+    "About": pg.show_about,
+    #"Examples": pg.show_examples,
+    #"Community": pg.show_community,
+}
+go_to = functions.get(page)
+if go_to:
+    go_to()
